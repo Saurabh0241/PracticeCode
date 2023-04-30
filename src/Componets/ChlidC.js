acff3daaaa78449a06aa1167cb46d83308ad822f
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-04-30 14:47:49 +0530

    Done

--- src/Componets/ChlidC.js
+++ src/Componets/ChlidC.js
@@ -1,31 +1,18 @@
-import { Fragment } from "react"
+import { Fragment, useContext } from "react"
 import React from "react"
 import { data , data1} from "./LearpropDrilling"
+
 const ChildC=()=>
 {
+   const Firstname=useContext(data);
+   const Lastname = useContext(data1)
     return(
         <Fragment>
-            <data.Consumer>
-            {
-                (name)=>{
-                    return(
-                       
-                        <data1.Consumer>
-                            {
-                                (name1)=>
-                                {
-                                    return(
-                                         <h1>My name is {name} {name1}</h1>
-
-                                    )
-                                }
-                            }
-
-                        </data1.Consumer>
-                    )
-                }
-            }
-        </data.Consumer>
+            
+            <h1>
+                Hi name is {Firstname} {Lastname}
+            </h1>
+       
         </Fragment>
     )
 }
--- src/Componets/LearpropDrilling.js
+++ src/Componets/LearpropDrilling.js
@@ -5,17 +5,19 @@ import { useState } from "react"
 import ChildA from "./ChildA"
 const data= createContext();
 const data1 = createContext();
-const LearnPropDrilling=({ })=>
+const LearnPropDrilling=()=>
 {
    const name ="Saurabh"
    const name1="Sakhare"
     return (
         <Fragment>  
-            <data.Provider value={name}>
-            <data1.Provider value={name1}>
-            <ChildA />
-            </data1.Provider>
-            </data.Provider>
+            <data.Provider value ={name}>
+                <data1.Provider value={name1}>
+                    <ChildA/>
+              </data1.Provider>
+                
+                </data.Provider>
+           
         </Fragment>
       
     )
